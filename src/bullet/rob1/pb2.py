d3bb072cb52fe0552d1fdb41d3b46a30f4289cb5
Andreas Kugel <andreas.kugel@web.de>
2024-02-25 17:41:58 +0100

    test1 gravity rotation ok

--- src/bullet/rob1/pb2.py
+++ src/bullet/rob1/pb2.py
@@ -54,8 +54,8 @@ if True:
     # Run the simulation
     for i in range(3000):
         p.stepSimulation()
-        
-        # Control the base-to-upper arm joint (shoulder-like motion)
+
+        # control joint rotations        
         shoulderPosition = math.sin(i * 0.01) * -0.3
         p.setJointMotorControl2(robotArmId, 0, p.POSITION_CONTROL, targetPosition=shoulderPosition)
         
@@ -65,11 +65,11 @@ if True:
         
         # Control the upper-to-lower arm joint (elbow-like motion)
         # The elbow's motion is typically dependent on the shoulder's position or a separate control scheme
-        if i < 1500:
-            elbowPosition = math.fabs(math.sin(i * 0.02))  # Simple example to flex and extend the elbow
-        else:
-            elbowPosition = math.fabs(math.sin(i * 0.01))  # Change the rate for demonstration
-        p.setJointMotorControl2(robotArmId, 2, p.POSITION_CONTROL, targetPosition=elbowPosition)
+##        if i < 1500:
+##            elbowPosition = math.fabs(math.sin(i * 0.02))  # Simple example to flex and extend the elbow
+##        else:
+##            elbowPosition = math.fabs(math.sin(i * 0.01))  # Change the rate for demonstration
+##        p.setJointMotorControl2(robotArmId, 2, p.POSITION_CONTROL, targetPosition=elbowPosition)
         
         time.sleep(1./240.)
 
---
+++ src/bullet/rob1/test1.py
@@ -0,0 +1,33 @@
+import pybullet as p
+import pybullet_data
+import time
+import math
+from PIL import Image
+import numpy as np
+import sys 
+
+# Connect to PyBullet
+p.connect(p.GUI)
+p.setGravity(0, 0, -10)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# Load the ground plane
+#planeId = p.loadURDF("plane.urdf")
+
+# Load the robot arm URDF
+# Ensure this path is correct for your setup
+robotArm = p.loadURDF("test1.urdf", [0, 0, 0], useFixedBase=0)
+
+# Set the target velocity to a non-zero value
+p.setJointMotorControl2(robotArm, 0, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
+
+
+# Run the simulation
+for i in range(3000):
+    p.stepSimulation()
+
+    
+    time.sleep(1./240.)
+
+p.disconnect()
+
