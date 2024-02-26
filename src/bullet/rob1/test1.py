542bf9218102f6f21c7dc825a095b9a06802258f
Andreas Kugel <andreas.kugel@web.de>
2024-02-26 07:48:20 +0100

    test with arm and hand

--- src/bullet/rob1/test1.py
+++ src/bullet/rob1/test1.py
@@ -14,12 +14,15 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # Load the ground plane
 #planeId = p.loadURDF("plane.urdf")
 
+#logId = p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "test1.webm")
+
 # Load the robot arm URDF
 # Ensure this path is correct for your setup
 robotArm = p.loadURDF("test1.urdf", [0, 0, 0], useFixedBase=0)
 
 # Set the target velocity to a non-zero value
 p.setJointMotorControl2(robotArm, 0, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
+p.setJointMotorControl2(robotArm, 1, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
 
 
 # Run the simulation
